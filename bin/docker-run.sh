b4f5db9dd5d9b4cf1336ec108d497fb0988f8213
Joe Chop <joseph.d.chop@gmail.com>
2020-01-22 14:07:45 -1000

    kibana upgrade

--- bin/docker-run.sh
+++ bin/docker-run.sh
@@ -4,11 +4,11 @@
 /usr/share/kibana/bin/kibana \
   -e $ELASTICSEARCH_URI \
   --server.host=$SERVER_HOST \
-  --server.basePath=$SERVER_BASEPATH \
-  --server.rewriteBasePath=false \
   --elasticsearch.username=$ELASTICSEARCH_KIBANA_USER \
   --elasticsearch.password=$ELASTICSEARCH_KIBANA_PASSWORD \
   --readonlyrest_kbn.custom_logout_link=$ROR_CUSTOM_LOGOUT_LINK \
   --readonlyrest_kbn.jwt_query_param="jwt" \
   --readonlyrest_kbn.session_timeout_minutes=525600 \
+  --server.basePath=$SERVER_BASEPATH \
+  --server.rewriteBasePath=true \
   --verbose
